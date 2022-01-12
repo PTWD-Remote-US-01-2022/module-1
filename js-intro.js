9f9c50b802a76017ba5b5fe0513424baa75a0bb9
sandrabosk <sandraboskovic@hotmail.com>
2022-01-12 03:07:47 +0100

    add lesson js intro - inital push

---
+++ js-intro.js
@@ -0,0 +1,75 @@
+// console.log("hi there PTWD Remote US");
+
+// var
+// let 
+// const
+
+// var. declaration
+let firstName;
+
+// console.log(firstName); // undefined
+
+// var. assignment (initialization)
+
+firstName = "marcos";
+
+console.log("firstName 1: ", firstName); // marcos
+
+let lastName = "palacios"; // var. declaration and initialization in the same line
+
+console.log(lastName); // palacios
+
+// var. can't be named with numbers, can't use any of the reserved JS words (function, class, constructor, ...)
+
+// 🚨 let 3students
+// 🚨 let const = ?
+
+let price, tax, total;
+
+// let price
+// let total
+
+
+// camelCase
+// snake_case
+
+
+let totalPrice = 300; // camelCase
+let total_price; // snake_case
+
+// we should write variables with lower case
+
+let address;
+
+// ❌ let Address ❌
+
+// comment 1
+// comment 2
+
+/*
+this is multiple
+lines
+comment
+*/
+
+
+// "const" is used when we are declaring a variable that won't change the value
+
+firstName = "stefan";
+
+console.log("firstName 2: " ,firstName);
+
+const fullName = "stefan culafic";
+
+console.log("fullName 1: ", fullName, "is a type of: ", typeof firstName);
+
+
+// fullName = "sandra boskovic";
+
+// console.log("fullName 2: ", fullName);
+
+firstName = 123;
+
+console.log("firstName as a number: ", firstName, "is a type of: ", typeof firstName);
+
+// ⭐️⭐️⭐️ variables declared with "const" can't be reassigned nor we can change their value ⭐️⭐️⭐️
