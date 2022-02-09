5dbffb7cd4a62cd20f96f7c3a9ed978b881eb0b4
sandrabosk <sandraboskovic@hotmail.com>
2022-02-09 04:03:12 +0100

    add solution of iteration 1 and 2

---
+++ lab-javascript-memory-game/js/index.js
@@ -0,0 +1,79 @@
+const cards = [
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' },
+  { name: 'aquaman', img: 'aquaman.jpg' },
+  { name: 'batman', img: 'batman.jpg' },
+  { name: 'captain america', img: 'captain-america.jpg' },
+  { name: 'fantastic four', img: 'fantastic-four.jpg' },
+  { name: 'flash', img: 'flash.jpg' },
+  { name: 'green arrow', img: 'green-arrow.jpg' },
+  { name: 'green lantern', img: 'green-lantern.jpg' },
+  { name: 'ironman', img: 'ironman.jpg' },
+  { name: 'spiderman', img: 'spiderman.jpg' },
+  { name: 'superman', img: 'superman.jpg' },
+  { name: 'the avengers', img: 'the-avengers.jpg' },
+  { name: 'thor', img: 'thor.jpg' }
+];
+
+const memoryGame = new MemoryGame(cards);
+
+console.log(memoryGame.cards);
+
+window.addEventListener('load', (event) => {
+  let html = '';
+  memoryGame.cards.forEach((pic) => {
+    html += `
+      <div class="card" data-card-name="${pic.name}">
+        <div class="back" name="${pic.img}"></div>
+        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+      </div>
+    `;
+  });
+
+  // Add all the divs to the HTML
+  document.querySelector('#memory-board').innerHTML = html;
+
+  // Bind the click event of each element to a function
+  document.querySelectorAll('.card').forEach((card) => {
+    card.addEventListener('click', () => {
+      // TODO: write some code here
+      // console.log(`Card clicked: ${card}`);
+
+      memoryGame.pickedCards.push(card);
+
+      // console.log(memoryGame.pickedCards);
+
+      if (memoryGame.pickedCards.length === 2){
+        const firstPicked = memoryGame.pickedCards[0];
+        const secondPicked = memoryGame.pickedCards[1];
+
+        // console.log(firstPicked, secondPicked);
+
+        const cardName1 = firstPicked.getAttribute("data-card-name");
+        const cardName2 = secondPicked.getAttribute("data-card-name");
+
+        // console.log(cardName1, cardName2);
+
+        if(memoryGame.checkIfPair(cardName1, cardName2)){
+          console.log("IT'S A MATCH!");
+
+          memoryGame.pickedCards = [];
+        } else {
+          console.log("if cards are not the same, flip them back")
+        }
+      }
+
+
+    });
+  });
+});
---
+++ lab-javascript-memory-game/js/memory.js
@@ -0,0 +1,54 @@
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = []; // the length of this arr is 2 and contains the cards we are comparing
+    this.pairsClicked = 0; // this number updates every 2 clicks
+    this.pairsGuessed = 0; // the goal is to get 12 pairs guessed
+    this.shuffleCards(cards);
+  }
+
+  // arr is a placeholder for array of cards we passed as an argument when we called the method in the constructor
+  shuffleCards(arr) {
+    let arrLength = arr.length;
+
+    while ( arrLength > 0 ){
+      // get random number from 0 to the length of the array - 1
+      let index = Math.floor(Math.random() * arrLength);
+  
+      arrLength --;
+  
+      // create a temporary variable that will store the last element in the arr
+      let tempVar = arr[arrLength];
+  
+  
+      // fill in the last el of the arr with some random element 
+      arr[arrLength] = arr[index];
+  
+      // fill in the empty. space where random element was with temp variable
+      arr[index] = tempVar;
+    }
+  
+    return arr;
+  }
+
+  checkIfPair(card1, card2) {
+
+    this.pairsClicked ++;
+
+    if (card1 === card2){
+      this.pairsGuessed ++;
+
+      return true;
+    } 
+
+    return false;
+    
+  }
+
+  checkIfFinished() {
+    // ... write your code here
+  }
+}
+
+// The following is required for automated testing. Please, ignore it.
+if (typeof module !== 'undefined') module.exports = MemoryGame;
